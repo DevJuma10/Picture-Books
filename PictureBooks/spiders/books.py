1464cf118915754ec39b7e3b5c45278dbb38e94d
Juma Melvine <juma.study@gmail.com>
2022-04-17 14:40:41 +0300

    first 12 items crawler

--- PictureBooks/spiders/books.py
+++ PictureBooks/spiders/books.py
@@ -1,10 +1,26 @@
 import scrapy
-
+import json
 
 class BooksSpider(scrapy.Spider):
     name = 'books'
-    allowed_domains = ['https://openlibrary.org/subjects/picture_books']
-    start_urls = ['http://https://openlibrary.org/subjects/picture_books/']
+    # allowed_domains = ['https://openlibrary.org/subjects/picture_books']
+    start_urls = ['https://openlibrary.org/subjects/picture_books.json?limit=12&offset=12']
 
     def parse(self, response):
-        pass
+        resp = json.loads(response.body)
+        works  = resp.get("works")
+
+        print(response.status)
+
+        for work in works:
+            yield{
+            "title" :  work.get("title"),
+            "author" : work.get("authors")[0].get("name")
+        }
+
+        # if response.status == 200:
+        #     yield scrapy.Request(
+        #         url =  f'https://openlibrary.org/subjects/picture_books.json?limit=12&offset=24',
+        #         callback = self.parse
+        #     )
+        
