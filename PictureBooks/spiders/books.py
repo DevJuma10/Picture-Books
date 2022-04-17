f2d2ba20439079194c9a35ac1a986266828b3453
Juma Melvine <juma.study@gmail.com>
2022-04-17 21:55:18 +0300

    handled pagination

--- PictureBooks/spiders/books.py
+++ PictureBooks/spiders/books.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy.exceptions import CloseSpider
 import json
 
 class BooksSpider(scrapy.Spider):
@@ -6,21 +7,32 @@ class BooksSpider(scrapy.Spider):
     # allowed_domains = ['https://openlibrary.org/subjects/picture_books']
     start_urls = ['https://openlibrary.org/subjects/picture_books.json?limit=12&offset=12']
 
+    INCREMENTED_BY = 12
+    offset = 0 
+
+
     def parse(self, response):
+
+        if response.status == 500:
+            raise CloseSpider("REACHED LAST PAGE")
+
+
         resp = json.loads(response.body)
         works  = resp.get("works")
-
+        
+    
         print(response.status)
 
         for work in works:
             yield{
-            "title" :  work.get("title"),
-            "author" : work.get("authors")[0].get("name")
+            "title" :  work.get("title"), 
+            # "author" : work.get("authors")[0].get("name")
         }
-
-        # if response.status == 200:
-        #     yield scrapy.Request(
-        #         url =  f'https://openlibrary.org/subjects/picture_books.json?limit=12&offset=24',
-        #         callback = self.parse
-        #     )
+        
+        
+        self.offset += self.INCREMENTED_BY
+        yield scrapy.Request(
+            url =  f'https://openlibrary.org/subjects/picture_books.json?limit=12&offset={self.offset}',
+            callback = self.parse
+        )
         
